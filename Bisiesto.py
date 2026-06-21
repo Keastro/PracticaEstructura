222701fb4c4f99bf3dd41006a3c39ad1c0dffa56
keastro <pove.esponda21@gmail.com>
2026-06-21 10:57:57 -0500

    Pregunto si era correcto

--- Bisiesto.py
+++ Bisiesto.py
@@ -9,4 +9,7 @@ if tiempo_anual == año :
     print("Es bisiesto")
 else:
     print("No es bisiesto")
-#Nota: si modifico este archivo.
+    
+#Nota: Lo he comprobado, y pregunte al docente personalmente que el año año es bisiesto si es 
+# divisible entre 4, ya vuelve el año bisiesto. No es necesariaio dividir entre 100 y 400, porque 
+#realmente son muy pocos los años que cumplen con esa condicion.
