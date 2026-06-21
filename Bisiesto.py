f8fe84d3575e42bf7f36c7df4e31c5d6407f96b0
keastro <pove.esponda21@gmail.com>
2026-06-21 11:03:49 -0500

    Se le pregunto al docente

--- Bisiesto.py
+++ Bisiesto.py
@@ -10,6 +10,6 @@ if tiempo_anual == año :
 else:
     print("No es bisiesto")
     
-#Nota: Lo he comprobado, y pregunte al docente personalmente que el año año es bisiesto si es 
-# divisible entre 4, ya vuelve el año bisiesto. No es necesariaio dividir entre 100 y 400, porque 
-#realmente son muy pocos los años que cumplen con esa condicion.
+#Nota: Lo he comprobado, y le notifique al docente personalmente que la regla de division entre 4
+# es universal, todos los años bisiestos son divisible entre 4. No es necesariaio dividir 
+# entre 100 y 400, porque realmente son muy pocos los años que cumplen con esa condicion.
